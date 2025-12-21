65fd069de84fd68b6d50d3530dc1a10667ea934b
harsh <harshbhati3759@gmail.comgit config --global user.name harshgit config --global user.email harshbhati3759@gmail.com>
2025-12-21 21:42:41 +0530

    Snake game - creating a snake

---
+++ snake_game.py
@@ -0,0 +1,45 @@
+import pygame
+
+pygame.init()
+
+#Set up the game window 
+window_width = 500
+window_height = 500
+window = pygame.display.set_mode((window_width, window_height))
+pygame.display.set_caption("Snake Game")
+
+#Set up the snake 
+snake_block_size = 10
+snake_speed = 15
+snake_list = []
+snake_length = 1
+snake_x = window_width / 2
+snake_y = window_height / 2
+snake_x_change = 0
+snake_y_change = 0
+
+#Define the function to draw the snake
+def draw_snake(snake_block_size, snake_list):
+    for block in snake_list:
+        pygame.draw.rect(window, "black", [block[0], block[1], snake_block_size, snake_block_size])
+
+#Start the game loop 
+run = True
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    #update the snake lisk
+    snake_head = []
+    snake_head.append(snake_x)
+    snake_head.append(snake_y)
+    snake_list.append(snake_head)
+
+    #Draw the game objects
+    window.fill("white")
+    draw_snake(snake_block_size, snake_list)
+    pygame.display.flip()
+
+pygame.quit()
+
